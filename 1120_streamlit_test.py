33922070985109e4203fb7f778c86d822530cf21
merits <yujaehoon@gmail.com>
2024-12-13 09:25:40 +0900

    Update 1120_streamlit_test.py

--- 1120_streamlit_test.py
+++ 1120_streamlit_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 
 #지도 위에 표시될 점 좌표 값을 위도경도에 담습니다 .
-base_position =  [37.5073423, 127.0572734]
+base_position =  [37.285438, 127.012756]
 #중심점의 위도, 경도 좌표를 리스트에 담습니다.
 
 # base_position에, 랜덤으로 생성한 값을 더하여 5개의 좌표를 데이터 프레임으로 생성하였고,
