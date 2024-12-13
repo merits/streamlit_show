f1255c56daa9941eb28d79445f03eadd72cf2f1b
merits <yujaehoon@gmail.com>
2024-12-13 12:27:44 +0900

    Update 1120_streamlit_test.py

--- 1120_streamlit_test.py
+++ 1120_streamlit_test.py
@@ -1,25 +1,46 @@
-import streamlit as st
-import folium
 import pandas as pd
+import numpy as np
+import folium
 
-# 하나의 좌표만 포함하도록 수정
-map_data = pd.DataFrame(
-    {
-        'lat': [37.7749],  # 위도
-        'lon': [-122.4194]  # 경도
-    }
-)
+# 데이터
+map_data = pd.DataFrame({
+    'lat': [-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
+    'lon': [-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
+    'name': ['Buenos Aires', 'Paris', 'Melbourne', 'St Petersburg', 
+    		 'Abidjan', 'Montreal', 'Nairobi', 'Salvador'],
+    'value': [10, 12, 40, 70, 23, 43, 100, 43] 
+  })
 
-# Folium을 사용하여 지도 생성
-def create_map(lat, lon):
-    # Folium 지도 생성
-    m = folium.Map(location=[lat, lon], zoom_start=13)
-    # 마커 추가
-    folium.Marker([lat, lon], popup='Marker').add_to(m)
-    return m
 
-# Streamlit 앱에서 지도 표시
-st.subheader('Folium Map')  # 제목 생성 
-map_object = create_map(map_data['lat'][0], map_data['lon'][0])  # Folium 지도 생성
-# Folium 지도를 HTML로 변환하여 Streamlit에 표시
-st.write(map_object._repr_html_(), unsafe_allow_html=True)
+#1 지도 객체 생성
+my_map = folium.Map(
+	location=[map_data['lat'].mean(), map_data['lon'].mean()], 
+    zoom_start=2)
+    
+    
+#2 지도 커스텀
+# 지도에 원형 마커와 값 추가
+for index, row in map_data.iterrows():       # 데이터프레임 한 행 씩 처리
+
+    folium.CircleMarker(                     # 원 표시
+        location=[row['lat'], row['lon']],   # 원 중심- 위도, 경도
+        radius=row['value'] / 5,             # 원의 반지름
+        color='pink',                        # 원의 테두리 색상
+        fill=True,                           # 원을 채움
+        fill_opacity=1.0                     # 원의 내부를 채울 때의 투명도
+    ).add_to(my_map)                         # my_map에 원형 마커 추가
+
+    folium.Marker(                           # 값 표시
+        location=[row['lat'], row['lon']],   # 값 표시 위치- 위도, 경도
+        icon=folium.DivIcon(
+        	html=f"<div>{row['name']} {row['value']}</div>"), # 값 표시 방식
+    ).add_to(my_map)                         # my_map에 값 추가
+    
+    
+#3 지도 제목과 캡션 추가
+st.title('Map with Location Data')
+st.caption(
+	"Displaying geographical data on a map using Streamlit and Folium")
+
+#4 지도 시각화
+st.components.v1.html(my_map._repr_html_(), width=800, height=600)
