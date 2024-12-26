f12a1db3afe0c1797b5b48bdb80a0c19644b9d8a
merits <yujaehoon@gmail.com>
2024-12-26 17:15:35 +0900

    Update 1120_streamlit_test.py

--- 1120_streamlit_test.py
+++ 1120_streamlit_test.py
@@ -1,15 +1,6 @@
-import speech_recognition as sr
+import pyaudio
 
-# microphone에서 auido source를 생성합니다
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-# 구글 웹 음성 API로 인식하기 
-try:
-    print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
-except sr.UnknownValueError as e:
-    print("Google Speech Recognition could not understand audio".format(e))
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+p = pyaudio.PyAudio()
+for i in range(p.get_device_count()):
+    info = p.get_device_info_by_index(i)
+    print(f"Device {i}: {info['name']}")
