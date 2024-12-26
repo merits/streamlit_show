e030c3d573e3f280580751e19fc3a35fcf652607
merits <yujaehoon@gmail.com>
2024-12-26 17:14:15 +0900

    Update 1120_streamlit_test.py

--- 1120_streamlit_test.py
+++ 1120_streamlit_test.py
@@ -1,19 +1,15 @@
-import streamlit as st
+import speech_recognition as sr
 
-def main():
-    st.title("타이핑 챗봇")
+# microphone에서 auido source를 생성합니다
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
 
-    # 챗봇 응답을 얻는 함수
-    def get_chatbot_response(user_input):
-        # 여기에서 실제로 챗봇의 응답 로직을 구현할 수 있습니다.
-        # 예를 들어, 간단하게 사용자 입력에 대한 답변을 생성할 수 있습니다.
-        return f"챗봇: '{user_input}'에 대한 답변입니다."
-
-    user_input = st.text_input("사용자 입력:")
-    
-    if st.button("대화하기"):
-        chatbot_response = get_chatbot_response(user_input)
-        st.text(chatbot_response)
-
-if __name__ == "__main__":
-    main()
+# 구글 웹 음성 API로 인식하기 
+try:
+    print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
+except sr.UnknownValueError as e:
+    print("Google Speech Recognition could not understand audio".format(e))
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
